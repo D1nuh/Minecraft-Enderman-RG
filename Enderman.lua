575bba52d656480d0b2a95ea625594052f262f41
Luiz Hilário <70781613+D1nuh@users.noreply.github.com>
2025-03-20 21:44:54 -0300

    Adicionando meu código a meu repositório

---
+++ Enderman.lua
@@ -0,0 +1,51 @@
+os.execute("chcp 65001")
+
+-- Criatura Geral
+local name = "Enderman"
+local description = "É um ser inteligente de aparencia alta, magra de cor escura que aparece durante a noite e que assim com o Slenderman te persegue"
+
+-- Atributos 
+local attack = 5
+local defence = 9
+local speed = 5
+
+
+-- função
+local function  Progresso(atributos)
+    local resultado = ""
+    for i = 1, 10, 1 do
+
+        -- tentei colocar originalmente esses emojis ❤️🖤 mas bugou foi tudo kkkkkk
+
+        if i <= atributos then
+
+            -- ponto de vida cheio
+            
+            resultado = resultado.."⬜"
+
+        else
+
+            -- ponto de vida vazio
+            
+            resultado = resultado.."⬛"
+        end
+    end
+
+    return resultado
+end
+
+print("")
+print("================================================")
+print("")
+print("- " .. name.. " -")
+print("- " .. description.. " -")
+print("-")
+print("- Atributos:")
+print("-    Ataque:".. Progresso(attack))
+print("-    Defesa:"..Progresso(defence))
+print("-    Velocidade"..Progresso(speed))
+print("-")
+print("-")
+print("-")
+print("-")
+print("================================================")
